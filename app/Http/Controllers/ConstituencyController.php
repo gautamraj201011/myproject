8f9957fc5e1faaaad18d5d9ce8f0c10c79997c60
gautamraj201011 <gautamraj201011@gmail.com>
2016-05-05 02:56:08 +0530

    3rd commit

---
+++ app/Http/Controllers/ConstituencyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\ConstituencyBlockDetail;
+
+class ConstituencyController extends Controller
+{
+    public function show()
+    { $users=DB::table('parties')->where('partyid','1')->first();
+
+        return view('parties.index',compact('users'));
+    }
+
+    public function create()
+    {
+        return view('constituency.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        foreach ($_POST['blocks'] as $names)
+        {
+           $blocks = new ConstituencyBlockDetail();
+            $blocks->electionid = $request->get('electionid');
+            $blocks->constituencyid = $request->get('constituencyid');
+            $users=DB::table('block_details')->where('blockid',$names+1)->first();
+            $blocks->block =$users->block;
+            $blocks->save();
+
+
+
+        }
+
+
+    }
+}
